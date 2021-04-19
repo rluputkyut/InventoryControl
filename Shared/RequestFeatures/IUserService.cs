3b4f8e267a1b3cf5850b0e56f6bfefed87bf4e73
Arkar <rluputkyut@gmail.com>
2021-04-20 00:25:40 +0800

    Update Authentication

---
+++ Shared/RequestFeatures/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControl.Shared.RequestFeatures
+{
+    public interface IUserService
+    {
+        public Task<User> LoginAsync(User user);
+        public Task<User> RegisterUserAsync(User user);
+        public Task<User> GetUserByAccessTokenAsync(string accessToken);
+        public Task<User> RefreshTokenAsync(RefreshRequest refreshRequest);
+    }
+}
---
+++ Shared/RequestFeatures/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControl.Shared.RequestFeatures
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string Source { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int PubId { get; set; }
+        public DateTime? HireDate { get; set; }
+
+        public Role Role { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+
+    public class Role
+    {
+        public short RoleId { get; set; }
+        public string RoleDesc { get; set; }
+    }
+
+    public class RefreshRequest
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
